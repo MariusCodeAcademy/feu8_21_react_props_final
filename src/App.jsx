c1fa8ee349a352b973b23f286b9dadc1aaaa396b
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-09-07 09:00:09 +0300

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,23 @@ function SingleCard(props) {
   console.log('card');
   console.log('props ===', props);
   //const props = {title: 'Monday', text: 'Is the first day'}
+  // <button>Do not click</button> paspaudimu iskviesti sayHi()
+  // <button>Do not click</button> paspaudimu iskviesti sayHi() ir perduoti varda pzd "James"
   return (
     <div>
       <h2>title: {props.title}</h2>
       <p>{props.text}</p>
+      <button>Do not click</button>
     </div>
   );
 }
 
 export default function App() {
   console.log('app');
+
+  function sayHi() {
+    console.log('hello there');
+  }
   return (
     <Container>
       <h1>New topic</h1>
